ae28e898c2633ef4de1c5447f82c5e208d272bcb
Bharti0294 <bharti.nehul07@gmail.com>
2021-04-08 10:30:47 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pre-Filled-From';
+  userData = {
+    name:'Bharti Devadhe',
+    email: 'bharti.nehul07@gmail.com',
+    
+    password: '123456',
+    address: '201 c wing, Pune Maharastra',
+    mobile: 9850419853
+    }
+    
 }
